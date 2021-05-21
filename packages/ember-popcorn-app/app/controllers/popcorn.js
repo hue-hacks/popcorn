fb408bb28ea0c7ee2969d122513d2b53df865cfe
Tim Lindvall <tlindval@linkedin.com>
2021-05-21 16:56:55 -0700

    feat: Popcorn iteration and shuffling.

--- packages/ember-popcorn-app/app/controllers/popcorn.js
+++ packages/ember-popcorn-app/app/controllers/popcorn.js
@@ -3,15 +3,40 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class PopcornController extends Controller {
+  @service('popcorn-store') popcornStore;
   @service timer;
 
+  get currentItem() {
+    return this.popcornStore.currentPopcorn.item;
+  }
+
+  get currentQueue() {
+    return this.popcornStore.currentPopcorn.queue;
+  }
+
+  get isPopcornRunning() {
+    return this.currentItem !== undefined;
+  }
+
   @action
-  startTimer() {
+  nextItem() {
+    this.popcornStore.currentPopcorn.personWent();
+    if (this.currentItem) {
+      this.timer.start();
+    } else {
+      this.timer.stop();
+    }
+  }
+
+  @action
+  start() {
+    this.popcornStore.currentPopcorn.startPopcorn();
     this.timer.start();
   }
 
   @action
-  stopTimer() {
+  end() {
+    this.popcornStore.currentPopcorn.endPopcorn();
     this.timer.stop();
   }
 }
